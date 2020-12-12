7685169f8432dc03a2c17d1f3c92ffd45ded168c
Alice <bigidiki143@gmail.com>
2020-12-12 20:51:56 +0500

    insertsort

---
+++ ArrayInsert/ArrayInsert/ArrayIns.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInsert
+{
+    class ArrayIns
+    {
+
+        private int nElems;
+        private int[] a = new int[10];
+
+        public void insertionSort()
+        {
+            int arrin;
+            int arrout;
+           int temp;
+
+            for (arrout = 1; arrout < nElems; arrout++)
+            {
+               temp = a[arrout];
+                arrin = arrout;
+                
+                while (arrin > 0 && a[arrin - 1] >= temp)
+                {
+                    a[arrin] = a[arrin - 1];
+                    --arrin;
+                }
+                a[arrin] = temp;
+               for(int i = 0; i < a.Length; i++)
+                {
+                    Console.Write(" {0} ",a[i]);
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+        public void insert(int value)
+        {
+            a[nElems] = value;
+            nElems++;
+        }
+        public  void display()
+        {
+
+            for (int j = 0; j < nElems; j++)
+            {
+                Console.Write(a[j] + " ");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ ArrayInsert/ArrayInsert/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInsert
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            ArrayIns ai;
+            ai = new ArrayIns();
+
+            ai.insert(88);
+            ai.insert(92);
+            ai.insert(77);
+            ai.insert(74);
+            ai.insert(32);
+            ai.insert(80);
+            ai.insert(95);
+            ai.insert(85);
+
+            ai.display();
+
+            ai.insertionSort();
+            ai.display();
+            Console.ReadLine();
+
+        }
+        
+      
+
+   
+    }
+}
